c382dc8e69709f6fcc65849bff2a2515da881ffe
alfonsolanderos06 <alfonso.landeros5047@alumnos.udg.mx>
2024-04-25 19:33:29 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://alfonsolanderos1006.github.io',
+    base: '/alfonsolanderos1006.github.io'
 });
